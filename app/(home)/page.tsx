b2d67ee92d5283ee10bea2320407ae390df045ed
PG <qkrrufgood@naver.com>
2024-08-08 16:42:52 +0900

    240808/4.3 PG

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,9 @@
-import Link from 'next/link';
-
+import Movie from '../../components/movie';
+import styles from "../../styles/home.module.css"
 export const metadata = {
   title:"Home"
 }
 export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
-
 
 async function getMovies() {
 //await new Promise((resolve)=> setTimeout(resolve,10));
@@ -13,14 +12,18 @@ async function getMovies() {
  return json;
 }
 
+interface IMovieProps{
+title:string;
+id:string;
+poster_path:string;
+}
+
 export default async function HomePage(){
   const movies = await getMovies();
   return(
-    <div>
+    <div className={styles.container}>
       {movies.map((movie) => (
-        <li key={movie.id}>
-          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
-        </li>
+        <Movie key={movie.id} id={movie.id} poster_path={movie.poster_path} title={movie.title}/>
       ))}
     </div>
   );
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,16 +1,22 @@
-"use client"
-import React from 'react';
-import Link from "next/link"
-import { usePathname } from "next/navigation"
+"use client";
 
-export default function Navigation(){
-  const path = usePathname();   
-  return(
-    <nav>
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import styles from "../styles/nav.module.css";
+
+export default function Navigation() {
+  const path = usePathname();
+  return (
+    <nav className={styles.nav}>
       <ul>
-        <li><Link href="/">Home</Link></li>{path ==="/"?"*":""}
-        <li><Link href="/about-us">About Us</Link></li>{path ==="/about-us"?"*":""}
+        <li>
+          <Link href="/">Home</Link> {path === "/" ? "🔥" : ""}
+        </li>
+        <li>
+          <Link href="/about-us">About Us</Link>
+          {path === "/about-us" ? "🔥" : ""}
+        </li>
       </ul>
     </nav>
-  )
+  );
 }
